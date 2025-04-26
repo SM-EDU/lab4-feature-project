86a182cbf0f0120813973d25fc5d2703e2bcaf10
sanprallc <sanprallc@gmail.com>
2025-04-26 12:27:32 -0700

    Added MyFirstProgram.java

---
+++ MyFirstProgram.java
@@ -0,0 +1,9 @@
+// Author: Santhil Murugesan
+// Lab 4 GitHub Feature Project
+// Basic java file, for testing git hub
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello from Lab 4!");
+    }
+}
